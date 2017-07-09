ffbeafbe5f148ddffa94be94fa29ddf9cd4b0e60
nikhilakki <nikhilakki@users.noreply.github.com>
2017-07-09 14:09:00 +0000

    Done

--- build.py
+++ build.py
@@ -1,30 +1,36 @@
-def join_dataframe(df1, df2):
-    pass
+import pandas as pd
+df1 = pd.read_csv('data/test_table.csv')
+df2 = pd.read_csv('data/user_table.csv')
 
+def join_dataframe(df1, df2):
+    df = pd.merge(df1, df2, how='left', on='user_id')
+    # df['age'] = df['age'].round().fillna(0.0).astype('int64')
+    # df['age'] = pd.cut(df['age'],bins=10, include_lowest=True)
+    return df
 
 def get_categorical_variables(df):
-    return []
+    df['test'] = df['test'].astype('object')
+    df['conversion'] = df['conversion'].astype('object')
+    return df.select_dtypes(include=['object'])
 
 
 def get_numerical_variables(df):
-    return []
+    return df.select_dtypes(exclude=['object'])
 
 
 def get_numerical_variables_percentile(df):
-    pass
+    return df.select_dtypes(exclude=['object']).describe()
 
 
 def get_categorical_variables_modes(df):
-    pass
-
+    return df.select_dtypes(include=['object']).mode()
 
 def get_missing_values_count(df):
-    pass
-
+    return df.isnull()
 
 def plot_histogram_with_numerical_values(df):
-    pass
+    return df.plot.hist()
 
 
 def plot_facet_box(df):
-    pass
+    return df.plot.box()
